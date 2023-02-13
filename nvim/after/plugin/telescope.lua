f8c73a799fc349de47cece5719439aafa43d81e8
Jorgen Jensvold <jorgenjensvold@gmail.com>
2023-02-13 13:24:42 +0000

    Create configuration files for new packages

---
+++ nvim/after/plugin/telescope.lua
@@ -0,0 +1,4 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})

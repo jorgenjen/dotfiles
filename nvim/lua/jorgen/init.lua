fe0c054cda25823a907a399bad170fc879d97d21
Jorgen Jensvold <jorgenjensvold@gmail.com>
2023-02-13 13:27:46 +0000

    Do a small restructure and change <Leader>

---
+++ nvim/lua/jorgen/init.lua
@@ -0,0 +1,10 @@
+require("jorgen.set") -- set file
+require("jorgen.remap") -- remap file
+--require("mason").setup() -- requires mason which is needed to install and update LSP servers
+
+--require('onedark').setup { -- color theme
+  --  style = 'darker'
+--}
+--require('onedark').load()
+
+-- print fish twice

13fb4ae99f67b43f1c6c4669c2110fc79627c859
Jorgen Jensvold <jorgenjensvold@gmail.com>
2023-02-13 15:07:23 +0000

    Add cmp completions
    Add cmp compeltions to config and configure it

--- nvim/lua/jorgen/packer.lua
+++ nvim/lua/jorgen/packer.lua
@@ -40,6 +40,17 @@ return require('packer').startup(function(use)
     use('mbbill/undotree')
 
 
+    -- cmp plugins - is used for autocompletion
+    use('hrsh7th/nvim-cmp') -- The compketion plugin
+    use('hrsh7th/cmp-buffer') -- buffer completions
+    use('hrsh7th/cmp-path') -- path completions
+    use('hrsh7th/cmp-cmdline') -- cmdline completions
+    use('saadparwaiz1/cmp_luasnip') -- snippet completions
+
+    -- snippets - the snippets that the autocompletion needs
+    use('L3MON4D3/LuaSnip') -- snippet engine
+    use('rafamadriz/friendly-snippets') -- Many snippets for many languages
+
  --   use ({
      --   "hrsh7th/nvim-cmp",
    --     config = [[require('config.cmp')]], -- may very based on config

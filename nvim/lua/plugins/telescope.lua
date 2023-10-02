2d9832ec62413c11870fb315597e2b7c9d48f54c
Jørgen Jensvold <jorgenjensvold@gmail.com>
2023-10-02 11:10:34 +0200

    Update telescope config to be used with lsp

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,11 +11,12 @@ return {
         -- File search and swapping -- using builtin functions to telescope
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- fuzzy file search
-        vim.keymap.set('n', '<leader>gg', builtin.git_files, {})  
-        vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})  -- Search for files based on content in the file
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Fuzzy file search"}) 
+        vim.keymap.set('n', '<leader>gg', builtin.git_files, {desc = "Fuzzy file search for all tracked files in the git repo"})
+        vim.keymap.set('n', '<leader>ss', builtin.live_grep, {desc = "Search for files based on content in the file"})
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, {desc = "Find word under cursor in current working directory"})
 
-        
+
         -- Setup for LSP utilizing telescope
         local telescope = require('telescope')
         local actions = require('telescope.actions')
@@ -23,11 +24,19 @@ return {
             defaults = {
                 mappings = {
                     i = {
-
+                        -- Keybindings in insert mode for this plugin
+                        ["<C-k>"] = actions.move_selection_previous, -- move to previous result
+                        ["C-j>"] = actions.move_selection_next,      -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                     }
                 }
             }
         })
-            
+
+        -- For improved telescope sorting performance
+        telescope.load_extension("fzf");
+
+
+
     end
 }

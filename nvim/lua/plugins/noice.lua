f14846966a5f4957aa8996438b7f0ae273a80a23
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-06 17:41:53 +0100

    Add plugin for central cmd line
    Attempt to remove all notifications that are too frequent

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,95 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallbac::k
+    "rcarriga/nvim-notify",
+  },
+
+  config = function(_)
+    require("noice").setup({
+      -- have filtered out moust things as I find notifications annoying and distracting
+        -- might not be the most efficient so remove plugin if you experience slowdowns
+        -- just here to make cmd line mor visually stunning and moves lua line futher down as cmdline is  removed
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "lines",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "line",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "before",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "after",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "cmdline",
+            find = "^:w<CR>$", -- Matches the ":w" command
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "lsp",
+            kind = "progress",
+            cond = function(message)
+              local client = vim.tbl_get(message.opts, "progress", "client")
+              return client == "lua_ls"
+            end,
+          },
+          opts = { skip = true },
+        },
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+      },
+    })
+  end,
+}

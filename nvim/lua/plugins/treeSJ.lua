106d533679a231a858711512291edf5b0d447e2f
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-08 18:30:54 +0100

    Remove dafault keymap to only have toggle

--- nvim/lua/plugins/treeSJ.lua
+++ nvim/lua/plugins/treeSJ.lua
@@ -3,7 +3,10 @@ return {
   -- keys = { '<space>m', '<space>j', '<space>s' },
   -- dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
-    require('treesj').setup({--[[ your config ]]})
-    vim.keymap.set("n", "<leader>s", require('treesj').toggle, {desc = "Toggle splitjoin of lists"})
+    require('treesj').setup({
+      ---@type boolean Use default keymaps (<space>m - toggle, <space>j - join, <space>s - split)
+      use_default_keymaps = false,
+    })
+    vim.keymap.set("n", "<leader>m", require('treesj').toggle, {desc = "Toggle splitjoin of lists"})
   end,
 }

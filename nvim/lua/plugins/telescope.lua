1c33036f01deb2074c7adde54a7a701404b45d3c
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-08 16:55:16 +0100

    Add event to hopefully remove error
    Sometimes when opening new reop telescope errors and requires reopneing
    not sure what causes it but loading at this event seems to make it
    better so far. Not sure if it is an actuall fix for the problem

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', -- tag = '0.1.3',
     branch = "0.1.x",
+    event = "BufWinEnter",
     dependencies = {
         'nvim-lua/plenary.nvim',
         {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}, -- for better performance 

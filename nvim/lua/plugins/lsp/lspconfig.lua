38a9d9087fff1eae72e856f2b79043340293bdf7
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-08 16:57:45 +0100

    Add lsp for cmake

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -113,6 +113,12 @@ return {
             on_attach = on_attach,
         })
 
+        -- cmake
+        lspconfig["cmake"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+
 
         -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({

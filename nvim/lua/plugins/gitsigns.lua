91740173d19aeeafe547291606c7fb77210d0d89
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-09 18:28:56 +0100

    Add binds for dealing with hunks

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,7 +12,7 @@ return {
             untracked    = { text = '┆' },
         },
         signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl` -- color lines according to git status
         linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
@@ -43,8 +43,56 @@ return {
         -- yadm = {
         --     enable = false
         -- },
-    },
+  },
+
+  config = function (_, opts)
+    require('gitsigns').setup{opts,
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts_inner)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts_inner)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']c', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[c', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
 
 
 
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk, {desc = "Stage hunk"})
+        map('n', '<leader>hr', gitsigns.reset_hunk, {desc = "Reset hunk"})
+        map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc = "Stage hunk"})
+        map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc = "Reset hunk"})
+        map('n', '<leader>hS', gitsigns.stage_buffer, {desc = "Stage buffer"})
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, {desc = "Undo stage hunk"})
+        map('n', '<leader>hR', gitsigns.reset_buffer, {desc = "Reset buffer"})
+        map('n', '<leader>hp', gitsigns.preview_hunk, {desc = "Preview hunk"})
+        map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, {desc = "Turn on git blame for line"})
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, {desc = "Toggle git blame for current line"})
+        map('n', '<leader>hd', gitsigns.diffthis, {desc = "Show hunk diff"})
+        map('n', '<leader>hD', function() gitsigns.diffthis('~') end, {desc = "Show hunk diff (Not sure how it's different)"})
+        map('n', '<leader>td', gitsigns.toggle_deleted, {desc = "Toggle deleted (IDK what that does)"})
+      end
+    }
+
+
+    -- require("scrollbar.handlers.gitsigns")
+  end
 }

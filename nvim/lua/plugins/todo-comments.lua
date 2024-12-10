69054a6bcf18e55ca7e8c510b3d5644346fe9d36
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-10 16:55:59 +0100

    Add search function for todo's

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -11,5 +11,9 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {}
+  opts = {},
+  config = function (opts)
+    require('todo-comments').setup()
+    vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", {silent = true, desc="Fuzzy search through todo's in work directory"})
+  end
 }

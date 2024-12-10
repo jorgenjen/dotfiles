5094a5ffb336bc4f0c7b729c7373998e36678bba
Jorgen Jensvold <jorgenjensvold@gmail.com>
2024-12-10 16:39:15 +0100

    Add TODO higlighting

---
+++ nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,15 @@
+-- PERF:  does not look like it should
+-- TODO:
+-- HACK:
+-- NOTE:
+-- FIX:
+-- WARNING:
+-- BUG:
+
+-- Plugin does what you can see above!
+
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {}
+}

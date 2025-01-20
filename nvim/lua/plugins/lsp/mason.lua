9c2fd456e994d8f2fe6cd126395c4c3ef6c9245e
Jorgen Jensvold <jorgenjensvold@gmail.com>
2025-01-20 16:55:50 +0100

    Move formatter and linter install
    Move to dependency and as part of mason.nvim

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,59 +1,78 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    --    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "clangd", -- C
-        --        "biome", -- JAvascript, json, typescript
-        -- "tsserver", -- Typesc, Javascript -- Seems to be deprecated
-        "ts_ls", -- typescript, javascript
-        "cssls", -- css
-        "html",
-        "pyright",
-        "emmet_ls",
-        "svelte",
-        "lua_ls", -- lua language server
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"clangd", -- C
+				--        "biome", -- JAvascript, json, typescript
+				-- "tsserver", -- Typesc, Javascript -- Seems to be deprecated
+				"ts_ls", -- typescript, javascript
+				"cssls", -- css
+				"html",
+				"pyright",
+				"emmet_ls",
+				"svelte",
+				"lua_ls", -- lua language server
 
-        -- does not attach to java files for some reason
-        --        "jdtls", -- java
-        --        "java_language_server",
+				-- does not attach to java files for some reason
+				--        "jdtls", -- java
+				--        "java_language_server",
 
+				--        "tsserver", -- typescript server
+				--        "html", -- html
+				--        "cssls", -- css
+				--        "tailwindcss", --
+				--        "svelte", -- Svelte
+				--        "lua_ls", -- lua
+				--        "emmet_ls", -- web dev snippets
+				--        "pyright", -- python
+			},
 
-        --        "tsserver", -- typescript server
-        --        "html", -- html
-        --        "cssls", -- css
-        --        "tailwindcss", --
-        --        "svelte", -- Svelte
-        --        "lua_ls", -- lua
-        --        "emmet_ls", -- web dev snippets
-        --        "pyright", -- python
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
-  end,
+			mason_tool_installer.setup({
+				ensure_installed = {
+					-- Formatters
+					"clang-format", -- c and cpp
+					"stylua", -- lua
+					"isort", -- python
+					"black", -- python
+					"prettierd", -- JS
+					"prettier", -- JS
+
+					-- Linters
+					"eslint_d", -- JS
+					"pylint", -- python
+					"cpplint", -- cpp and c static code analysis
+					-- "bacon", -- rust -- fails to install
+					"cmakelint", -- cmake
+					"shellcheck", -- bash
+				},
+			}),
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+	end,
 }

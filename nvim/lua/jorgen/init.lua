0da6cd0617ce392aaf150b43e55c0822241abd49
Jorgen Jensvold <jorgenjensvold@gmail.com>
2025-02-03 12:32:19 +0100

    Formater modifications

--- nvim/lua/jorgen/init.lua
+++ nvim/lua/jorgen/init.lua
@@ -4,34 +4,33 @@ require("jorgen.remap")
 -- loads and installs lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 --require("lazy").setup("plugins")
 
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
---  install = {
---    colorscheme = { "nightfly" },
---  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	--  install = {
+	--    colorscheme = { "nightfly" },
+	--  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
 --require("lazy").setup("plugins", "plugins.lsp")
 --require("lazy").setup({{import = "plugins", import = "plugins.lsp"}})
-
 
 -- Removes higlighting of linenumber of the marked line (by mark.nvim)
 vim.api.nvim_set_hl(0, "MarkSignNumHL", { link = "Comment" })
@@ -41,5 +40,5 @@ vim.api.nvim_set_hl(0, "MarkSignHL", { fg = "#C678DD" }) -- set to the pink colo
 
 -- turn off diagnostic in signcolumn
 vim.diagnostic.config({
-  signs = false,
+	signs = false,
 })
